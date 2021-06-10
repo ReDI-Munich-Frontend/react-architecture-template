fe258c2a0adce979cc93a1cecb549c66c7db7d91
Savio van Hoi <savio.vanhoi@gmail.com>
2021-06-10 19:45:01 +0200

    chore: clean up project and add basic routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/numbers/100">Numbers 100</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/numbers/:numberId">
+            <Number />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Hello class of 2021</h2>;
+}
+
+function Number() {
+  const [numberValue, setNumberValue] = useState("");
+  const { numberId } = useParams();
+
+  useEffect(() => {
+    fetch(`http://numbersapi.com/${numberId}`)
+      .then((res) => res.text())
+      .then((data) => setNumberValue(data));
+  }, [numberId]);
+
+  return (
+    <h2>
+      {numberId} - {numberValue}
+    </h2>
+  );
+}
